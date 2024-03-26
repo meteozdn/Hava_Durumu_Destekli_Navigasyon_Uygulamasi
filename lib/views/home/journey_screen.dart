865536541650460f4545c1d5e5e0ac81e25858b8
Metehan Özden <metehanozden.bil@gmail.com>
2024-03-27 00:17:44 +0300

    Ana sayfaları ekler

---
+++ lib/views/home/journey_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JourneyScreen extends StatelessWidget {
+  const JourneyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      child: const Center(
+        child: Text("Journey"),
+      ),
+    );
+  }
+}
---
+++ lib/views/home/navigation_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavigationScreen extends StatelessWidget {
+  const NavigationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: const Center(
+        child: Text("Navigation"),
+      ),
+    );
+  }
+}
