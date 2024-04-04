fd925cf47d9b5232780b5e2a7dfb3348e1fdf0ab
burakmey <burak_meydan-gs@hotmail.com>
2024-04-04 16:21:12 +0300

    Main screen added as initial screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:navigationapp/views/home/home_screen.dart';
+import 'package:navigationapp/views/main/main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,9 +14,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       home: Scaffold(
-        body: HomeScreen(),
+        body: MainScreen(),
       ),
     );
   }
