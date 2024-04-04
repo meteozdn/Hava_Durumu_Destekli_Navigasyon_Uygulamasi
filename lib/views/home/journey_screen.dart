184e164316f68fd4011c7474f825aeff4c8df9dc
burakmey <burak_meydan-gs@hotmail.com>
2024-04-04 17:47:58 +0300

    Listing for user routes.

--- lib/views/home/journey_screen.dart
+++ lib/views/home/journey_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:navigationapp/core/controllers/route_controller.dart';
 
 class JourneyScreen extends StatelessWidget {
-  const JourneyScreen({super.key});
+  const JourneyScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
-      child: const Center(
-        child: Text("Journey"),
+    return Scaffold(
+      body: GetX<RouteController>(
+        init: Get.find<RouteController>(),
+        builder: (routeController) {
+          return ListView.builder(
+            itemCount: routeController.routes.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: const Icon(Icons.person),
+                title: Text(routeController.routes[index].id),
+                onTap: () {},
+              );
+            },
+          );
+        },
       ),
     );
   }
