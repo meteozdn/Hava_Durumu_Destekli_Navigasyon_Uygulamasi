4687de272c14f11f9d5ee56b7b4bf3ef48e6706e
burakmey <burak_meydan-gs@hotmail.com>
2024-04-13 17:43:16 +0300

    FriendScreen added.

---
+++ lib/views/profile/friends_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:navigationapp/core/controllers/friend_request_controller.dart';
+import 'package:navigationapp/views/profile/search_user_screen.dart';
+
+class FriendsScreen extends StatelessWidget {
+  const FriendsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+            onPressed: () {
+              Get.to(() => SearchUserPage());
+            },
+            child: const Text("Arkadaş ekle")),
+        const Text("Arkadaşlık istekleri"),
+        Expanded(
+          child: GetX<FriendRequestController>(
+            init: Get.find<FriendRequestController>(),
+            builder: (controller) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: controller.friendRequestsIn.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: const Icon(Icons.person),
+                          title: Text(controller
+                              .friendRequestsIn[index].senderUsername),
+                          onTap: () {},
+                          trailing: ElevatedButton(
+                            onPressed: () async {
+                              await controller.buttonAction(
+                                  userId: controller
+                                      .friendRequestsIn[index].senderId,
+                                  buttonText: "Accept Request");
+                            },
+                            child: const Text("Accept Request"),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: controller.friendRequestsOut.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: const Icon(Icons.person),
+                          title: Text(controller
+                              .friendRequestsOut[index].recipientUsername),
+                          onTap: () {},
+                          trailing: ElevatedButton(
+                            onPressed: () async {
+                              await controller.buttonAction(
+                                  userId: controller
+                                      .friendRequestsOut[index].recipientId,
+                                  buttonText: "Remove Request");
+                            },
+                            child: const Text("Remove Request"),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  const Text("Arkadaşlar"),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: controller.friends.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: const Icon(Icons.route),
+                          title: Text(controller.friends[index].id),
+                          onTap: () {},
+                        );
+                      },
+                    ),
+                  )
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
