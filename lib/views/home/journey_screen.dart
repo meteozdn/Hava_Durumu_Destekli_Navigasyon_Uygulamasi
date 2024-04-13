5c9eda73ea4aa5e55cd4bbdd561305d0e9246ca8
burakmey <burak_meydan-gs@hotmail.com>
2024-04-13 17:42:40 +0300

    Updating JourneyScreen.

--- lib/views/home/journey_screen.dart
+++ lib/views/home/journey_screen.dart
@@ -7,22 +7,30 @@ class JourneyScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GetX<RouteController>(
-        init: Get.find<RouteController>(),
-        builder: (routeController) {
-          return ListView.builder(
-            itemCount: routeController.routes.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: const Icon(Icons.person),
-                title: Text(routeController.routes[index].id),
-                onTap: () {},
+    return Column(
+      children: [
+        const Text("Yolculuğa başla"),
+        ElevatedButton(onPressed: () {}, child: const Text("Şimdi başla")),
+        ElevatedButton(onPressed: () {}, child: const Text("Rota planla")),
+        const Text("Kayıtlı Rotalar"),
+        Expanded(
+          child: GetX<RouteController>(
+            init: Get.find<RouteController>(),
+            builder: (controller) {
+              return ListView.builder(
+                itemCount: controller.routes.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: const Icon(Icons.route),
+                    title: Text(controller.routes[index].id),
+                    onTap: () {},
+                  );
+                },
               );
             },
-          );
-        },
-      ),
+          ),
+        ),
+      ],
     );
   }
 }
