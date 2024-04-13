84233d34415b88c1b1eeed4ad1ea19c6139b5215
burakmey <burak_meydan-gs@hotmail.com>
2024-04-13 17:40:39 +0300

    Updating ChatGroupController.

--- lib/core/controllers/chat_group_controller.dart
+++ lib/core/controllers/chat_group_controller.dart
@@ -5,13 +5,43 @@ import 'package:navigationapp/core/controllers/user_controller.dart';
 import 'package:navigationapp/core/models/chat_group.dart';
 
 class ChatGroupController extends GetxController {
-  var chatGroups = <ChatGroup>[].obs;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final RxList<ChatGroup> _chatGroups = <ChatGroup>[].obs;
+  List<ChatGroup> get chatGroups => _chatGroups;
 
   @override
   void onInit() async {
     super.onInit();
     await fetchUserChatGroups();
+  }
+
+  Future<void> fetchUserChatGroups() async {
+    try {
+      chatGroups.clear();
+      String currentUserId = Get.find<UserController>().user.value!.id;
+      List<String> ids =
+          Get.find<UserController>().user.value!.chatGroups ?? [];
+      // Get ChatGroups from firestore.
+      for (String id in ids) {
+        DocumentSnapshot snapshot = await firestore
+            .collection(FirestoreCollections.chatGroups)
+            .doc(id)
+            .get();
+        // Create ChatGroup models.
+        ChatGroup chatGroup = ChatGroup.fromFirestore(snapshot);
+        // If the ChatGroup is for two persons, the name will be friend's username.
+        if (chatGroup.name.isEmpty) {
+          String friendId =
+              chatGroup.members.firstWhere((id) => id != currentUserId);
+          Get.find<UserController>().fetchUser(userId: friendId).then((value) {
+            chatGroup.name = value.username;
+          });
+        }
+        chatGroups.add(chatGroup);
+      }
+    } catch (error) {
+      throw Exception(error.toString());
+    }
   }
 
   Future<void> createChatGroup({
@@ -24,14 +54,15 @@ class ChatGroupController extends GetxController {
       // Create ChatGroup model.
       ChatGroup chatGroup =
           ChatGroup(id: id, name: name, members: members, sharedRoutes: []);
+      // Save ChatGroup to local.
+      Get.find<UserController>().user.value!.chatGroups?.add(id);
       // Save ChatGroup to fireStore.
       await firestore
           .collection(FirestoreCollections.chatGroups)
           .doc(id)
           .set(chatGroup.toJson());
       for (String userId in members) {
-        await Get.find<UserController>()
-            .updateUserChatGroupList(chatGroupId: id, userId: userId);
+        await updateUserChatGroupList(chatGroupId: id, userId: userId);
         await fetchUserChatGroups();
       }
     } catch (error) {
@@ -39,30 +70,27 @@ class ChatGroupController extends GetxController {
     }
   }
 
-  Future<void> fetchUserChatGroups() async {
+  Future<void> updateUserChatGroupList(
+      {required String chatGroupId,
+      required String userId,
+      bool isAdding = true}) async {
     try {
-      List<String> ids =
-          Get.find<UserController>().user.value!.chatGroups ?? [];
-      List<ChatGroup> chatGroups = [];
-      // Get ChatGroups from firestore.
-      for (String id in ids) {
-        DocumentSnapshot snapshot = await firestore
-            .collection(FirestoreCollections.chatGroups)
-            .doc(id)
-            .get();
-        // Create ChatGroup models.
-        ChatGroup chatGroup = ChatGroup.fromFirestore(snapshot);
-        // If the ChatGroup is for two persons, the name will be friend's username.
-        if (chatGroup.name.isEmpty) {
-          String friendId = chatGroup.members.firstWhere(
-              (id) => id != Get.find<UserController>().user.value!.id);
-          Get.find<UserController>().fetchUser(userId: friendId).then((value) {
-            chatGroup.name = value.username;
-          });
-        }
-        chatGroups.add(chatGroup);
+      // Save to firestore.
+      if (isAdding) {
+        await firestore
+            .collection(FirestoreCollections.users)
+            .doc(userId)
+            .update({
+          "chatGroups": FieldValue.arrayUnion([chatGroupId])
+        });
+      } else {
+        await firestore
+            .collection(FirestoreCollections.users)
+            .doc(userId)
+            .update({
+          "chatGroups": FieldValue.arrayRemove([chatGroupId])
+        });
       }
-      this.chatGroups.value = chatGroups;
     } catch (error) {
       throw Exception(error.toString());
     }
