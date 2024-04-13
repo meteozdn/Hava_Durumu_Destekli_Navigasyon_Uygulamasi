87abd6b7849ccceae044669dd804e0bf165d666f
burakmey <burak_meydan-gs@hotmail.com>
2024-04-13 17:40:59 +0300

    Updating FriendRequestController.

--- lib/core/controllers/friend_request_controller.dart
+++ lib/core/controllers/friend_request_controller.dart
@@ -4,21 +4,73 @@ import 'package:navigationapp/core/constants/firestore_collections.dart';
 import 'package:navigationapp/core/controllers/chat_group_controller.dart';
 import 'package:navigationapp/core/controllers/user_controller.dart';
 import 'package:navigationapp/core/models/friend_request.dart';
+import 'package:navigationapp/core/models/user.dart';
 
 class FriendRequestController extends GetxController {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final RxList<FriendRequest> _friendRequestsIn = <FriendRequest>[].obs;
+  List<FriendRequest> get friendRequestsIn => _friendRequestsIn;
+  final RxList<FriendRequest> _friendRequestsOut = <FriendRequest>[].obs;
+  List<FriendRequest> get friendRequestsOut => _friendRequestsOut;
+  final RxList<User> _friends = <User>[].obs;
+  List<User> get friends => _friends;
 
-  void sendFriendRequest({
-    required String senderId,
-    required String recipientId,
-  }) async {
+  @override
+  void onInit() async {
+    super.onInit();
+    await fetchFriendRequests();
+  }
+
+  Future<void> fetchFriendRequests() async {
+    try {
+      friendRequestsIn.clear();
+      friendRequestsOut.clear();
+      friends.clear();
+      // Store user data to local.
+      String currentUserId = Get.find<UserController>().user.value!.id;
+      await Get.find<UserController>()
+          .setAuthenticatedUser(userId: currentUserId);
+      // Store friends in local.
+      List<String> friendIds =
+          Get.find<UserController>().user.value!.friends ?? [];
+      for (String id in friendIds) {
+        User friend = await Get.find<UserController>().fetchUser(userId: id);
+        friends.add(friend);
+      }
+      // Fetch both incoming and outgoing friend requests.
+      QuerySnapshot incomingRequestsSnapshot = await firestore
+          .collection(FirestoreCollections.friendRequests)
+          .where("recipientId", isEqualTo: currentUserId)
+          .get();
+      QuerySnapshot outgoingRequestsSnapshot = await firestore
+          .collection(FirestoreCollections.friendRequests)
+          .where("senderId", isEqualTo: currentUserId)
+          .get();
+      // Combine both incoming and outgoing friend requests.
+      for (var doc in incomingRequestsSnapshot.docs) {
+        friendRequestsIn.add(FriendRequest.fromFirestore(doc));
+      }
+      for (var doc in outgoingRequestsSnapshot.docs) {
+        friendRequestsOut.add(FriendRequest.fromFirestore(doc));
+      }
+    } catch (error) {
+      throw Exception(error.toString());
+    }
+  }
+
+  Future<void> sendFriendRequest(
+      {required String senderId,
+      required String senderUsername,
+      required String recipientId,
+      required String recipientUsername}) async {
     try {
       // Create friendRequest model.
       FriendRequest friendRequest = FriendRequest(
-        id: senderId + recipientId,
-        senderId: senderId,
-        recipientId: recipientId,
-      );
+          id: senderId + recipientId,
+          senderId: senderId,
+          senderUsername: senderUsername,
+          recipientId: recipientId,
+          recipientUsername: recipientUsername);
       // Save friendRequest to Firestore.
       await firestore
           .collection(FirestoreCollections.friendRequests)
@@ -29,22 +81,20 @@ class FriendRequestController extends GetxController {
     }
   }
 
-  void acceptFriendRequest({
+  Future<void> acceptFriendRequest({
     required String senderId,
     required String recipientId,
   }) async {
     try {
       // Add sender to recipient's friend list.
-      Get.find<UserController>()
-          .updateUserFriendList(userId: recipientId, friendId: senderId);
+      await updateUserFriendList(userId: recipientId, friendId: senderId);
       // Add recipient to sender's friend list.
-      Get.find<UserController>()
-          .updateUserFriendList(userId: senderId, friendId: recipientId);
+      await updateUserFriendList(userId: senderId, friendId: recipientId);
       // Create chatGroup.
-      Get.find<ChatGroupController>()
+      await Get.find<ChatGroupController>()
           .createChatGroup(members: [senderId, recipientId]);
       // Delete the friend request document.
-      deleteFriendRequest(senderId: senderId, recipientId: recipientId);
+      await deleteFriendRequest(senderId: senderId, recipientId: recipientId);
     } catch (error) {
       throw Exception(error.toString());
     }
@@ -55,6 +105,7 @@ class FriendRequestController extends GetxController {
     required String recipientId,
   }) async {
     try {
+      // Remove friendRequest from Firestore.
       await firestore
           .collection(FirestoreCollections.friendRequests)
           .doc(senderId + recipientId)
@@ -64,47 +115,61 @@ class FriendRequestController extends GetxController {
     }
   }
 
-  Future<String> getFriendState({required String friendId}) async {
-    List<Future<void>> futures = [];
-    String text = "AddFriend";
-    // If user already sent friend request.
-    futures.add(_getFriendRequestSnapshot(
-            senderId: Get.find<UserController>().user.value!.id,
-            recipientId: friendId)
-        .then((value) {
-      if (value!.docs.isNotEmpty) {
-        text = "RemoveRequest";
-      }
-    }));
-    // If user already received friend request.
-    futures.add(_getFriendRequestSnapshot(
-            senderId: friendId,
-            recipientId: Get.find<UserController>().user.value!.id)
-        .then((value) {
-      if (value!.docs.isNotEmpty) {
-        text = "AcceptRequest";
-      }
-    }));
-    futures.add(Get.find<UserController>()
-        .fetchUser(userId: Get.find<UserController>().user.value!.id)
-        .then((value) {
-      if (value.friends!.contains(friendId)) {
-        text = "RemoveFriend";
-      }
-    }));
-    await Future.wait(futures);
-    return text;
+  Future<void> buttonAction(
+      {required String userId, required String buttonText}) async {
+    String currentUserId = Get.find<UserController>().user.value!.id;
+    switch (buttonText) {
+      case "Remove Request":
+        await deleteFriendRequest(senderId: currentUserId, recipientId: userId);
+        break;
+      case "Accept Request":
+        await acceptFriendRequest(senderId: userId, recipientId: currentUserId);
+        break;
+      case "Remove Friend":
+        await updateUserFriendList(
+            userId: currentUserId, friendId: userId, isAdding: false);
+        await updateUserFriendList(
+            userId: userId, friendId: currentUserId, isAdding: false);
+        break;
+      case "Add Friend":
+        String recipientUsername = await Get.find<UserController>()
+            .fetchUser(userId: userId)
+            .then((value) => value.username);
+        await sendFriendRequest(
+            senderId: currentUserId,
+            senderUsername: Get.find<UserController>().user.value!.username,
+            recipientId: userId,
+            recipientUsername: recipientUsername);
+        break;
+      default:
+    }
+    await fetchFriendRequests();
   }
 
-  Future<QuerySnapshot?> _getFriendRequestSnapshot({
-    required String senderId,
-    required String recipientId,
-  }) async {
-    QuerySnapshot querySnapshot = await firestore
-        .collection(FirestoreCollections.friendRequests)
-        .where("senderId", isEqualTo: senderId)
-        .where("recipientId", isEqualTo: recipientId)
-        .get();
-    return querySnapshot;
+  // Method to update users friends list.
+  Future<void> updateUserFriendList(
+      {required String userId,
+      required String friendId,
+      bool isAdding = true}) async {
+    try {
+      // Save to firestore.
+      if (isAdding) {
+        await firestore
+            .collection(FirestoreCollections.users)
+            .doc(userId)
+            .update({
+          "friends": FieldValue.arrayUnion([friendId])
+        });
+      } else {
+        await firestore
+            .collection(FirestoreCollections.users)
+            .doc(userId)
+            .update({
+          "friends": FieldValue.arrayRemove([friendId])
+        });
+      }
+    } catch (error) {
+      throw Exception(error.toString());
+    }
   }
 }
