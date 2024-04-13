5cbb56d9cea5c8df7cc994791d29a8fea9a379a2
burakmey <burak_meydan-gs@hotmail.com>
2024-04-13 17:43:30 +0300

    MessagesScreen added.

---
+++ lib/views/profile/messages_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessagesScreen extends StatelessWidget {
+  const MessagesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: const Center(
+        child: Text("Messages"),
+      ),
+    );
+  }
+}
