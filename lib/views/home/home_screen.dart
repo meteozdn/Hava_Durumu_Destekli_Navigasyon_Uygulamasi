36121ed83430b2a86ffec9af2212d5c9944a7d3e
burakmey <burak_meydan-gs@hotmail.com>
2024-04-13 17:42:19 +0300

    Updating HomeScreen.

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:navigationapp/views/home/journey_screen.dart';
 import 'package:navigationapp/views/home/navigation_screen.dart';
 import 'package:navigationapp/views/home/weather_screen.dart';
+import 'package:navigationapp/views/profile/profile_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
-  RxInt _index = 1.obs;
-  final List<String> titles = ["Hava Durumu", "Navigasyon", "Yolcluk"];
+  HomeScreen({Key? key}) : super(key: key);
+  final RxInt _index = 1.obs;
+  final List<String> titles = ["Hava Durumu", "Navigasyon", "Yolculuk"];
 
   void indexController(int num) {
     _index(num);
@@ -17,15 +17,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<Widget> screens = [
-      WeatherScreen(),
-      NavigationScreen(),
-      JourneyScreen(),
+      const WeatherScreen(),
+      const NavigationScreen(),
+      const JourneyScreen(),
     ];
-
     return Obx(() {
       return Scaffold(
         appBar: AppBar(
           title: Text(titles[_index.value]),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.person),
+              onPressed: () {
+                // Navigate to the profile screen
+                Get.to(() => ProfileScreen());
+              },
+            ),
+          ],
         ),
         body: screens[_index.value],
         bottomNavigationBar: bottomNav(),
