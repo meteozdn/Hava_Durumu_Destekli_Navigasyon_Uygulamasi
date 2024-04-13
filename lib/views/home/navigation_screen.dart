25c8e7ab0a88e5e2fd25c112a08d559895d60e3d
burakmey <burak_meydan-gs@hotmail.com>
2024-04-13 17:43:01 +0300

    Updating NavigationScreen.

--- lib/views/home/navigation_screen.dart
+++ lib/views/home/navigation_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:navigationapp/core/controllers/route_controller.dart';
 
 class NavigationScreen extends StatelessWidget {
   const NavigationScreen({super.key});
@@ -7,9 +12,20 @@ class NavigationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
-      child: const Center(
-        child: Text("Navigation"),
+      child: Center(
+        child: MaterialButton(
+          onPressed: addRoute,
+          color: Colors.deepPurple,
+          child: const Text("Random Route"),
+        ),
       ),
     );
+  }
+
+  void addRoute() async {
+    var destinationLocation =
+        GeoPoint(Random().nextDouble(), Random().nextDouble());
+    await Get.find<RouteController>()
+        .createRoute(destinationLocation: destinationLocation);
   }
 }
