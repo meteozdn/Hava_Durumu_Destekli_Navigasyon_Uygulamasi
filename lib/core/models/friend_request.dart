8b0c7ca044b6a4bfcd77171b872992ba37af5b33
burakmey <burak_meydan-gs@hotmail.com>
2024-04-13 17:39:44 +0300

    New variables added.

--- lib/core/models/friend_request.dart
+++ lib/core/models/friend_request.dart
@@ -3,22 +3,32 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FriendRequest {
   final String id;
   final String senderId;
+  final String senderUsername;
   final String recipientId;
+  final String recipientUsername;
 
-  FriendRequest({
-    required this.id,
-    required this.senderId,
-    required this.recipientId,
-  });
+  FriendRequest(
+      {required this.id,
+      required this.senderId,
+      required this.senderUsername,
+      required this.recipientId,
+      required this.recipientUsername});
 
   factory FriendRequest.fromFirestore(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return FriendRequest(
         id: data["id"],
         senderId: data["senderId"],
-        recipientId: data["recipientId"]);
+        senderUsername: data["senderUsername"],
+        recipientId: data["recipientId"],
+        recipientUsername: data["recipientUsername"]);
   }
 
-  Map<String, dynamic> toJson() =>
-      {"id": id, "senderId": senderId, "recipientId": recipientId};
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "senderId": senderId,
+        "senderUsername": senderUsername,
+        "recipientId": recipientId,
+        "recipientUsername": recipientUsername
+      };
 }
