d3fad57a829a5b3a7c59b5f046da3914a67f2918
Metehan Özden <metehanozden.bil@gmail.com>
2024-05-22 21:03:17 +0300

    ‘init’

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:navigationapp/core/constants/app_constants.dart';
 import 'package:navigationapp/views/home/home_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(const MainApp());
 }
 
@@ -14,10 +13,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: Scaffold(
-        body: HomeScreen(),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(400, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          //     title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+                backgroundColor: ColorConstants.pictionBlueColor),
+            textTheme: Typography.englishLike2018.apply(
+              bodyColor: ColorConstants.blackColor,
+              fontSizeFactor: 1.sp,
+            ),
+          ),
+          home: child,
+        );
+      },
+      child: HomeScreen(),
     );
   }
 }
--- lib/views/home/navigation_screen.dart
+++ lib/views/home/navigation_screen.dart
@@ -1,14 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:navigationapp/core/constants/app_constants.dart';
 
 class NavigationScreen extends StatelessWidget {
   const NavigationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: const Center(
-        child: Text("Navigation"),
+    late GoogleMapController mapController;
+    final LatLng _center = LatLng(39, 32);
+
+    _onMapCreated(GoogleMapController controller) {
+      mapController = controller;
+    }
+
+    return Stack(
+      children: [
+        GoogleMap(
+          initialCameraPosition: CameraPosition(target: _center, zoom: 8.h),
+          onMapCreated: _onMapCreated, compassEnabled: true,
+          myLocationButtonEnabled: false,
+
+          // mapType: MapType.hybrid,
+          //myLocationButtonEnabled: true,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 60.0.h, left: 30.w),
+          child: const ElevatedWidgetButton(
+            image: WeatherIcons.sunny,
+            height: 30,
+            width: 70,
+            text: '40',
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ElevatedWidgetButton extends StatelessWidget {
+  const ElevatedWidgetButton({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.text,
+    required this.image,
+  });
+  final int width;
+  final String text;
+  final String image;
+  final int height;
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(10.r),
+      elevation: 3.h,
+      child: SizedBox(
+        width: width.w,
+        height: height.h,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+                height: 20.w,
+                child: Image.asset(
+                  image,
+                  height: 20.h,
+                )),
+            Text(
+              "$text°",
+              style: AppTextStyle.midBlack,
+            )
+          ],
+        ),
       ),
     );
   }
