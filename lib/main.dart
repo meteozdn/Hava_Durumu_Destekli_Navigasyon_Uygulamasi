ca1c66c6b7e640a38cb3b90545a34a8c585202f5
Metehan Özden <metehanozden.bil@gmail.com>
2024-05-22 22:26:16 +0300

    ‘init’

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:navigationapp/core/constants/app_constants.dart';
+import 'package:navigationapp/firebase_options.dart';
 import 'package:navigationapp/views/home/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
