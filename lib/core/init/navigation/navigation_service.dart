935d1ef313ae8b54e537147d0bb4c7a4b5412176
Metehan Özden <metehanozden.bil@gmail.com>
2024-05-23 23:33:51 +0300

    message page

--- lib/core/init/navigation/navigation_service.dart
+++ lib/core/init/navigation/navigation_service.dart
@@ -2,7 +2,7 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:navigationapp/core/constants/navigation_constants.dart';
 import 'package:navigationapp/export.dart';
 import 'package:navigationapp/views/home/home_screen.dart';
-import 'package:navigationapp/views/message/message_view.dart';
+import 'package:navigationapp/views/message/message_list_view.dart';
 
 class NavigationService {
   static List<GetPage> routes = [
--- lib/views/message/message_view.dart
+++ lib/views/message/message_view.dart
@@ -1,26 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:navigationapp/core/constants/app_constants.dart';
+import 'package:flutter/cupertino.dart';
 
-class MessageView extends StatelessWidget {
-  const MessageView({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 30.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: ColorConstants.pictionBlueColor,
-                  iconColor: ColorConstants.whiteColor),
-              onPressed: () {},
-              child: const Icon(Icons.person_add_alt_1),
-            ),
-          )
-        ],
-      ),
-    );
+    return Container();
   }
 }
