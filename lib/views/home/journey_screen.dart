c5c5b7be3ebf0c3fb746e4075faa4256428b19ef
burakmey <burak_meydan-gs@hotmail.com>
2024-05-23 23:16:48 +0300

    Owned and shared routes added.

--- lib/views/home/journey_screen.dart
+++ lib/views/home/journey_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:navigationapp/controllers/route_controller.dart';
 import 'package:navigationapp/views/components/indicator.dart';
 import 'package:navigationapp/views/components/journey_card.dart';
 
@@ -102,53 +104,72 @@ class JourneyScreen extends StatelessWidget {
               },
               controller: _controller,
               children: [
-                ListView.builder(
-                    padding:
-                        EdgeInsets.only(left: 20.w, right: 20.w, bottom: 30),
-
-                    //  padding: EdgeInsets.only(bottom: 20.0.h),
-                    itemCount: journeys.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Padding(
-                        padding: EdgeInsets.only(top: 20.0.h),
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.r),
-                          elevation: 10.h,
-                          child: JourneyWidget(
-                            isMY: true,
-                            startCity: journeys[index].user,
-                            endCity: 'Samsun',
-                            startDate: '18.00',
-                            endDate: '20.30',
-                            user: 'Ahmet Mehmet',
-                            time: '12 sa 10 dk',
-                          ),
-                        ),
+                Expanded(
+                  child: GetX<RouteController>(
+                    init: Get.find<RouteController>(),
+                    builder: (controller) {
+                      return ListView.builder(
+                        padding: EdgeInsets.only(left: 20.w, right: 20.w),
+                        itemCount: controller.sharedRoutes.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.only(top: 20.0.h),
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20.r),
+                              elevation: 10.h,
+                              child: JourneyWidget(
+                                isMY: true,
+                                startCity:
+                                    controller.sharedRoutes[index].startingCity,
+                                endCity: controller
+                                    .sharedRoutes[index].destinationCity,
+                                startDate: controller
+                                    .sharedRoutes[index].plannedAt
+                                    .toString(),
+                                endDate: '(estimated time?)',
+                                user: controller.sharedRoutes[index].ownerName,
+                                time: '12 sa 10 dk',
+                              ),
+                            ),
+                          );
+                        },
                       );
-                    }),
-                ListView.builder(
-                    padding: EdgeInsets.only(left: 20.w, right: 20.w),
-
-                    //   padding: EdgeInsets.only(bottom: 20.0.h),
-                    itemCount: journeys.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Padding(
-                        padding: EdgeInsets.only(top: 20.0.h),
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.r),
-                          elevation: 10.h,
-                          child: JourneyWidget(
-                            isMY: false,
-                            startCity: journeys[index].user,
-                            endCity: 'Samsun',
-                            startDate: '18.00',
-                            endDate: '20.30',
-                            user: 'Ahmet Mehmet',
-                            time: '12 sa 10 dk',
-                          ),
-                        ),
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: GetX<RouteController>(
+                    init: Get.find<RouteController>(),
+                    builder: (controller) {
+                      return ListView.builder(
+                        padding: EdgeInsets.only(left: 20.w, right: 20.w),
+                        itemCount: controller.userRoutes.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.only(top: 20.0.h),
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20.r),
+                              elevation: 10.h,
+                              child: JourneyWidget(
+                                isMY: true,
+                                startCity:
+                                    controller.userRoutes[index].startingCity,
+                                endCity: controller
+                                    .userRoutes[index].destinationCity,
+                                startDate: controller
+                                    .userRoutes[index].plannedAt
+                                    .toString(),
+                                endDate: '(estimated time?)',
+                                user: controller.userRoutes[index].ownerName,
+                                time: '12 sa 10 dk',
+                              ),
+                            ),
+                          );
+                        },
                       );
-                    }),
+                    },
+                  ),
+                ),
               ],
             ),
           ),
