8a8828b37eb3b9ddc6ea2eab0a714a7c0cb6b7df
burakmey <burak_meydan-gs@hotmail.com>
2024-05-23 23:09:36 +0300

    Main screen added.

---
+++ lib/views/main/main_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:navigationapp/controllers/auth_controller.dart';
+import 'package:navigationapp/controllers/chat_group_controller.dart';
+import 'package:navigationapp/controllers/friend_request_controller.dart';
+import 'package:navigationapp/controllers/route_controller.dart';
+import 'package:navigationapp/controllers/user_controller.dart';
+import 'package:navigationapp/views/auth/auth_screen.dart';
+import 'package:navigationapp/views/home/home_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GetX<AuthController>(
+        init: AuthController(),
+        builder: (authController) {
+          Get.put(UserController());
+          if (authController.user.value != null) {
+            return _buildAuthenticatedScreens(authController);
+          } else {
+            return AuthScreen();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildAuthenticatedScreens(AuthController authController) {
+    return FutureBuilder<void>(
+      future: _initializeControllers(authController.user.value!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return _buildLoadingScreen();
+        } else {
+          return HomeScreen();
+        }
+      },
+    );
+  }
+
+  Future<void> _initializeControllers(String userId) async {
+    final userController = Get.find<UserController>();
+    await userController.setAuthenticatedUser(userId: userId);
+
+    Future chatGroupControllerFuture = Future(() {
+      Get.put(ChatGroupController());
+    });
+    await Future.wait([chatGroupControllerFuture]);
+    Get.put(RouteController());
+    Get.put(FriendRequestController());
+  }
+
+  Widget _buildLoadingScreen() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
