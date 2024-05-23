61ac03f47ce79ca3f1275f27d53e7918aa4182ef
burakmey <burak_meydan-gs@hotmail.com>
2024-05-23 23:12:39 +0300

    Firestore collection names added.

---
+++ lib/core/constants/firestore_collections.dart
@@ -0,0 +1,7 @@
+class FirestoreCollections {
+  static const String chatGroups = "chatGroups";
+  static const String friendRequests = "friendRequests";
+  static const String messages = "messages";
+  static const String routes = "routes";
+  static const String users = "users";
+}
