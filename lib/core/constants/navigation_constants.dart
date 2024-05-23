947c8f71037de9fd40dfb6b319be75ffbc7354de
burakmey <burak_meydan-gs@hotmail.com>
2024-05-23 23:13:15 +0300

    Auth and main navigations added.

--- lib/core/constants/navigation_constants.dart
+++ lib/core/constants/navigation_constants.dart
@@ -1,4 +1,6 @@
 class NavigationConstants {
+  static const main = "/main";
+  static const auth = "/auth";
   static const home = "/home";
   static const message = "/message";
   static const changeLanguage = "/changeLanguage";
