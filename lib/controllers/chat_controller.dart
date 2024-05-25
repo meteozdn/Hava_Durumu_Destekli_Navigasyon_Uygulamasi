969e4f869a08be42176013f9c0e95532a133898a
burakmey <burak_meydan-gs@hotmail.com>
2024-05-25 16:59:46 +0300

    Message seen function added.

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:navigationapp/controllers/user_controller.dart';
+import 'package:navigationapp/core/constants/firestore_collections.dart';
 import 'package:navigationapp/models/message.dart';
 import 'package:navigationapp/services/chat_service.dart';
 
@@ -9,6 +11,7 @@ class ChatController extends GetxController {
   final ChatService _chatService = ChatService();
   final TextEditingController messageController = TextEditingController();
   final RxList<Message> messages = <Message>[].obs;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   RxBool isLoading = true.obs;
 
   ChatController(this.chatGroupId);
@@ -36,6 +39,23 @@ class ChatController extends GetxController {
       await _chatService.sendMessage(
           chatGroupId: chatGroupId, text: messageController.text);
       messageController.clear();
+    }
+  }
+
+  Future<void> seeMessage() async {
+    try {
+      // Update the "seen" field of the last message.
+      String userId = Get.find<UserController>().user.value!.id;
+      await firestore
+          .collection(FirestoreCollections.chatGroups)
+          .doc(chatGroupId)
+          .collection(FirestoreCollections.messages)
+          .doc(messages.last.id)
+          .update({
+        "seen": FieldValue.arrayUnion([userId])
+      });
+    } catch (error) {
+      throw Exception(error.toString());
     }
   }
 }
