735c5bb48a59d9557cf4bd8a7de03fb695e83572
burakmey <burak_meydan-gs@hotmail.com>
2024-06-12 21:18:59 +0300

    Listen firebase for sharedRoutes.

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -1,25 +1,59 @@
+import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:navigationapp/controllers/route_controller.dart';
 import 'package:navigationapp/controllers/user_controller.dart';
 import 'package:navigationapp/core/constants/firestore_collections.dart';
+import 'package:navigationapp/models/chat_group.dart';
 import 'package:navigationapp/models/message.dart';
 import 'package:navigationapp/services/chat_service.dart';
 
 class ChatController extends GetxController {
+  ChatController(this.chatGroupId);
+
   final String chatGroupId;
   final ChatService _chatService = ChatService();
+  Rx<ChatGroup?> chatGroup = Rx<ChatGroup?>(null);
+
   final TextEditingController messageController = TextEditingController();
   final RxList<Message> messages = <Message>[].obs;
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   RxBool isLoading = true.obs;
 
-  ChatController(this.chatGroupId);
+  StreamSubscription<DocumentSnapshot>? chatGroupSubscription;
+  DocumentReference? chatGroupDocRef;
 
   @override
   void onInit() async {
     super.onInit();
     loadMessages();
+    chatGroupDocRef =
+        _firestore.collection(FirestoreCollections.chatGroups).doc(chatGroupId);
+    listenToChatGroupUpdates();
+  }
+
+  @override
+  void onClose() {
+    chatGroupSubscription?.cancel();
+    super.onClose();
+  }
+
+  void listenToChatGroupUpdates() {
+    chatGroupSubscription = chatGroupDocRef?.snapshots().listen((snapshot) {
+      if (snapshot.exists) {
+        ChatGroup newChatGroupData = ChatGroup.fromFirestore(snapshot);
+        ChatGroup? oldChatGroupData = chatGroup.value;
+        chatGroup.value = newChatGroupData;
+        if (oldChatGroupData == null) {
+          return;
+        }
+        // Check for updates to "sharedRoutes" field.
+        if (oldChatGroupData.sharedRoutes != newChatGroupData.sharedRoutes) {
+          Get.find<RouteController>().fetchUserRoutes();
+        }
+      }
+    });
   }
 
   void loadMessages() {
@@ -46,7 +80,7 @@ class ChatController extends GetxController {
     try {
       // Update the "seen" field of the last message.
       String userId = Get.find<UserController>().user.value!.id;
-      await firestore
+      await _firestore
           .collection(FirestoreCollections.chatGroups)
           .doc(chatGroupId)
           .collection(FirestoreCollections.messages)
